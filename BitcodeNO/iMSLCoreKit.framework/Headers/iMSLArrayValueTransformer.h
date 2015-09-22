706f11e2b69914d2f6bc89283b6863c7b7b21eee
Milan Horvatovic <milan.horvatovic@gmail.com>
2015-09-22 14:49:24 +0200

    --

---
+++ BitcodeNO/iMSLCoreKit.framework/Headers/iMSLArrayValueTransformer.h
@@ -0,0 +1,31 @@
+//
+//  iMSLArrayValueTransformer.h
+//  iMSLCoreKit
+//
+//  Created by Milan Horvatovic on 17/07/15.
+//  Copyright (c) 2015 iMSoft Labs. All rights reserved.
+//
+
+#import "iMSLValueTransformer.h"
+
+@interface iMSLArrayValueTransformer : iMSLValueTransformer
+
+@end
+
+#pragma mark - NSString
+@interface iMSLArrayOfStringValueTransformer : iMSLArrayValueTransformer
+
+@end
+
+@interface iMSLStringArrayValueTransformer : iMSLArrayValueTransformer
+
+@end
+
+#pragma mark - NSNumber
+@interface iMSLArrayOfNumberValueTransformer : iMSLArrayValueTransformer
+
+@end
+
+@interface iMSLNumberArrayValueTransformer : iMSLArrayValueTransformer
+
+@end
